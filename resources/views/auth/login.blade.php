2e4fc195fded9d4123087000b598adc3f7296082
ikhwanma <ikhwan749@gmail.com>
2021-05-19 12:36:37 +0700

    ubah logo

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,7 +1,7 @@
 <x-guest-layout>
     <x-jet-authentication-card>
         <x-slot name="logo">
-            <x-jet-authentication-card-logo />
+            <h3>MANJA.in</h3>
         </x-slot>
 
         <x-jet-validation-errors class="mb-4" />
