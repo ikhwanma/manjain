b75f9286e50cc8035b9ca852ed9f5e59a32d217c
ikhwanma <ikhwan749@gmail.com>
2021-05-27 16:06:51 +0700

    tes

--- resources/views/buatLowongan.blade.php
+++ resources/views/buatLowongan.blade.php
@@ -1,69 +1,37 @@
-<!doctype html>
-<html lang="en">
-
+<!DOCTYPE html>
+<html>
 <head>
-    <!-- Required meta tags -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-
-    <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
-
-    <title>Buat Lowongan</title>
+<!-- Fonts -->
+<link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
+ 
+<!-- Styles -->
+<link rel="stylesheet" href="../css/style.css">
 </head>
-
+ 
 <body>
-    <div class="container">
-        <h2 class="alert alert-primary text-center mt-3">Buat Lowongan Pekerjaan</h2>
-
-        <form>
-            <div class="form-group">
-                <label>Judul Lowongan</label>
-                <input type="text" name="" class="form-control" placeholder="Masukkan Judul Lowongan Anda">
-            </div>
-
-            <div class="form-group">
-                <label>Kategori Pekerjaan</label>
-                <input type="text" name="" class="form-control" placeholder="Masukkan Kategori Pekerjaan">
-            </div>
-
-            <div class="form-group">
-                <label>Alamat Kantor</label>
-                <input type="text" name="" class="form-control" placeholder="Masukkan Alamat Kantor Anda">
-            </div>
-
-            <div class="form-group">
-                <label>Gaji</label>
-                <input type="text" name="" class="form-control" placeholder="Masukkan Nominal Gaji">
-            </div>
-
-            <div class="form-group">
-                <label>Jadwal Kerja</label>
-                <input type="text" name="" class="form-control" placeholder="Masukkan Jadwal Kerja">
-            </div>
-
-            <div class="form-group">
-                <label for="DeskripsiPerusahaan">Deskripsi Perusahaan</label>
-                <textarea class="form-control" name="" id="DeskripsiPerusahaan" rows="3" placeholder="Masukkan Deskripsi Perusahaan Anda"></textarea>
-            </div>
-
-            <div class="form-group">
-                <label for="CaraMelamar">Cara Melamar</label>
-                <textarea class="form-control" name="" id="CaraMelamar" rows="3" placeholder="Masukkan Cara Melamar"></textarea>
-            </div>
-
-            <button type="submit" class="btn btn-primary">Submit</button>
-            <button type="reset" class="btn btn-danger">Reset</button>
-
-        </form>
-
+@if(Session::get('success'))
+    <div class="alertSuccess">
+        {{Session::get('success')}}
     </div>
-    
-    <!-- Optional JavaScript -->
-    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+@endif
+ 
+<h1 style="text-align: center;">Buat Lowongan</h1>
+<form action="addLowongan" method="post">
+    @csrf
+    Judul Lowongan :  <input type="text" name="judul" placeholder="Masukkan Judul" value="{{old('judul')}}">
+    <span style="color: red;">@error('judul'){{$message}} @enderror</span><br>
+    Kategori Pekerjaan : <input type="text" name="kategori" placeholder="Masukkan Kategori" value="{{old('kategori')}}">
+    <span style="color: red;">@error('kategori'){{$message}} @enderror</span><br>
+    Gaji : <input type="text" name="gaji" placeholder="Masukkan Gaji" value="{{old('gaji')}}"> 
+    <span style="color: red;">@error('gaji'){{$message}} @enderror</span><br>
+    Jadwal Kerja : <input type="text" name="jadwal" placeholder="Masukkan jadwal" value="{{old('jadwal')}}"> 
+    <span style="color: red;">@error('jadwal'){{$message}} @enderror</span><br>
+    Deskripsi Perusahaan : <textarea name="deskripsi"  cols="30" rows="6" placeholder="Masukkan Deskripsi Perusahaan" value="{{old('deskrpsi')}}"></textarea>
+    <span style="color: red;">@error('deskripsi'){{$message}} @enderror</span><br>
+    Cara Melamar: <textarea name="cara"  cols="30" rows="6" placeholder="Masukkan Cara Melamar" value="{{old('deskrpsi')}}"></textarea><br>
+    <span style="color: red;">@error('cara'){{$message}} @enderror</span><br>
+<input type="submit" name="button" class="inputp" value="Submit">
+</form>
+ 
 </body>
-
 </html>
